ecdfe93f0bb317bd02b0915c48bc28c4d143629f
Stefan Lundgren <stefan.lundgren@hiq.se>
2018-01-17 21:27:16 +0100

    EKO-22 Migrate news view

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,18 +1,34 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, AfterViewInit} from '@angular/core';
 import {MenuService} from '../_services/menu.service';
+
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
-export class NewsComponent implements OnInit {
+export class NewsComponent implements OnInit, AfterViewInit {
 
-  constructor(private menuService: MenuService) {
+  constructor(
+      private menuService: MenuService) {
   }
 
   ngOnInit() {
     this.menuService.activeRoute = 'News';
   }
 
+  ngAfterViewInit () {
+    !function(d,s,id){
+        var js: any,
+            fjs=d.getElementsByTagName(s)[0],
+            p='https';
+        if(!d.getElementById(id)){
+            js=d.createElement(s);
+            js.id=id;
+            js.src=p+"://platform.twitter.com/widgets.js";
+            fjs.parentNode.insertBefore(js,fjs);
+        }
+    }
+    (document,"script","twitter-wjs");
+}
 }
