6a470ec58e0436d6d437b28d0bc88e58087bc7de
Alan Tibbetts <alan.tibbets@arbetsformedlingen.se>
2018-02-21 15:51:12 +0100

    [EKO-149] Switch to PhantomJS (ChromeHeadless refuses to work).

--- karma.conf.js
+++ karma.conf.js
@@ -8,15 +8,16 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
@@ -28,32 +29,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
 
-    browsers: ['Chromium'],
-    browsers: ['ChromiumHeadless'],
-    browsers: ['Chrome'],
-    browsers: ['ChromeHeadless'],
-
-    customLaunchers: {
-           ChromeHeadless:  {
-              base:   'Chrome',
-              flags:  [
-                '--headless',
-                '--disable-gpu',
-                '--remote-debugging-port=9222',
-              ],
-            },
-
-            ChromiumHeadless:  {
-              base:   'Chrome',
-              flags:  [
-                '--headless',
-                '--disable-gpu',
-                '--remote-debugging-port=9222',
-              ],
-            }
-
-      },
-      browsers: ['Chrome', 'ChromeHeadless', 'Chromium', 'ChromiumHeadless'],
+    browsers: ['Chrome', 'PhantomJS'],
     singleRun: false
   });
 };
